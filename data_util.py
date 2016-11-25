446b7d4c6cb3aedbccb974e2bce07facd3a3678d
AlphaLFC <398505136@qq.com>
2016-11-25 16:26:01 +0800

    mention pair embedding & scoring network done but not tested

--- data_util.py
+++ data_util.py
@@ -1,7 +1,7 @@
 #coding=utf-8
 from compiler.ast import flatten
-
-
+import polyglot
+from polyglot.text import Text
 class DataUtil:
     def __init__(self, config):
         self.config = config
@@ -12,41 +12,81 @@ class DataUtil:
         self.sentences = []
         self.all_word_average = 0
         self.type_dict = {}
+        self.init_data()
+
+    def init_data(self):
+        self.build_line_dict()
+        self.parse_data()
+        all_words = self.get_all_words()
+        print all_words
+        print self.sentences
+        self.calc_word_average(all_words)
 
     def mention_pos(self, mention):
         line = self.sentences[mention[0]]
         m_count = len([word for word in line if 'n' in word or word == 't'])
-        return (mention[1]+1)/m_count
+        return [(mention[1]+1)/m_count*0.1]
 
     def distance_mentions(self, m1, m2):
-        return abs(m1[1]-m2[1])
+        return [abs(m1[1]-m2[1])]
+
+    def mention_equals(m1,m2):
+        return m1[0]==m2[0] and m1[1]==m2[1] and m1[2]==m2[2] and m1[3]==m2[3] 
+
+    def distance_intervening_mentions(self, m1, m2):
+        c = 0
+        start = False
+        for m in self.mentions:
+            if self.mention_equals(m,m1):
+                start = True
+            if self.mention_equals(m,m2):
+                break
+            if start:
+                c += 1
+        return [c]
+
+    def same_speaker(self, m1, m2):
+        return [1]
 
     def is_overlap(self, m1, m2):
-        if m1[2]==m2[2]:
-            return 1
-        return 0
+        if m1[2] == m2[2]:
+            return [1]
+        return [0]
 
-    def calc_all_word_average(self):
+    def get_all_words(self):
         all_words = flatten(self.sentences)
-        #TODO
+        return all_words
+
+    def calc_word_average(self, words):
+        for word in words:
+            if word != '':
+                print word
+                vec = Text(word).words[0].vector
+                print vec
+        average = sum([Text(word).words[0].vector for word in words if word != ''])/len(words)*1.0
+        return [average]
 
     def build_line_dict(self):
-        with open('results.txt') as f:
+        with open(self.config.result_path) as f:
             lines = f.readlines()
             for line in lines:
                 line_num, word_index = line.split()
                 self.line_dict[int(line_num)] = int(word_index)
 
-    def find_first_word(self, mention):
-        line_num = mention[0]
-        return self.sentences[line_num][0]
+    def find_first_word_embedding(self, mention):
+        line = self.sentences[mention[0]]
+        assert line!=[]
+        return Text(line[0]).words[0].vector
 
-    def find_last_word(self, mention):
-        line_num = mention[0]
-        return self.sentences[line_num][-1]
+
+    def find_last_word_embedding(self, mention):
+        line = self.sentences[mention[0]]
+        assert line!=[]
+        return Text(line[-1]).words[0].vector
 
     def find_following(self, mention, word_num):
         line= self.sentences[mention[0]]
+        assert line!=[]
         word_index = mention[1]
         if word_index >= len(line)-word_num:
             for i in range(word_num-(len(line)-1-word_index)):
@@ -56,6 +96,7 @@ class DataUtil:
 
     def find_proceding(self, mention, word_num):
         line = self.sentences[mention[0]]
+        assert line!=[]
         word_index = mention[1]
         if word_index <= word_num - 1:
             for i in range(word_num-word_index):
@@ -65,10 +106,11 @@ class DataUtil:
 
     def average_sent(self, mention):
         line = self.sentences[mention[0]]
-        #TODO
+        assert line!=[]
+        return calc_word_average(line)
 
-    def read_feed(self):
-        with open('out.txt') as o_f:
+    def parse_data(self):
+        with open(self.config.data_path) as o_f:
             lines = o_f.readlines()
             for k in range(len(self.line_dict.items())):
                 line_num, word_index = self.line_dict.items()[k]
@@ -95,9 +137,8 @@ class DataUtil:
                 else:
                     self.sentences.append([])
 
-
-
-
+    def build_feed_dict(self, start, end):
+        return self.mentions[start:end], self.Ts[start:end], self.As[start:end]
 
 
 
--- trainer.py
+++ trainer.py
@@ -1,8 +1,11 @@
 import argparse
 # Import data
 import tensorflow as tf
+from compiler.ast import flatten
 from data_util import DataUtil
 from config import Config
+import polyglot
+from polyglot.text import Text
 
 
 class Coref_clustter:
@@ -22,7 +25,37 @@ class Coref_clustter:
         self.Ts = tf.placeholder([])
 
     def h(self,a,m):
-        return tf.constant(tf.random_normal([self.config.I, 1]))
+        embed_a = Text(a).vector
+        embed_m = Text(m).vector
+        first_aw_embed = self.du.find_first_word_embedding(a)
+        first_mw_embed = self.du.find_first_word_embedding(m)
+        last_aw_embed = self.du.find_last_word_embedding(a)
+        last_mw_embed = self.du.find_last_word_embedding(m)
+        proced2_a_embed = flatten([Text(word).words[0].vector for word in self.du.find_proceding(a,2) if word != ''])
+        follow2_a_embed = flatten([Text(word).words[0].vector for word in self.du.find_following(a,2) if word != ''])
+        proced2_m_embed = flatten([Text(word).words[0].vector for word in self.du.find_proceding(m,2) if word != ''])
+        follow2_m_embed = flatten([Text(word).words[0].vector for word in self.du.find_following(m,2) if word != ''])
+        avg5f_a = self.du.calc_word_average(self.du.find_following(a, 5))
+        avg5p_a = self.du.calc_word_average(self.du.find_proceding(a,5))
+        avg5f_m = self.du.calc_word_average(self.du.find_following(m, 5))
+        avg5p_m = self.du.calc_word_average(self.du.find_proceding(m,5))
+        avgsent_a = self.du.average_sent(a)
+        avgsent_m = self.du.average_sent(m)
+        avg_all = [self.du.all_word_average]
+
+        type_a = 0# self.du.type_dict[a[3]]
+        type_m =0# self.du.type_dict[m[3]]
+        mention_pos_a = self.du.mention_pos(a)
+        mention_pos_m = self.du.mention_pos(m)
+        mention_len_a = len(a[2])
+        mention_len_m = len(b[2])
+
+        distance = self.du.distance_mentions(a,m)
+        distance_m = self.du.distance_intervening_mentions(a,m)
+
+        result = embed_a + first_aw_embed + last_aw_embed + proced2_a_embed + follow2_a_embed + avg5f_a + avg5p_a + avgsent_a + type_a + mention_pos_a + mention_len_a + embed_m + first_mw_embed + last_mw_embed + proced2_m_embed + follow2_m_embed + avg5f_m + avg5p_m + avgsent_m + type_m + mention_pos_m + mention_len_m +  avg_all + distance + distance_m 
+
+        return tf.expand_dims(tf.constant(result),1)
 
     def r(self,a,m):
         h1 = tf.nn.relu(tf.matmul(self.W1, self.h(a, m)) + self.b1)
@@ -44,9 +77,9 @@ class Coref_clustter:
             return self.config.a_wl
         return 0
 
-    def read_feed(self):
-        return {}
-
+    def read_feed(self, start, end):
+         M, Ts, As = self.du.build_feed_dict
+         return {self.M:M, self.Ts:Ts, self.As:As}
 
     def main(self):
         loss = 0
@@ -70,8 +103,8 @@ class Coref_clustter:
         sess = tf.InteractiveSession()
         # Train
         tf.initialize_all_variables().run()
-        for _ in range(1000):
-            feed = self.du.read_feed()
+        for i in range(20):
+            feed = self.du.read_feed(self.config.batch_size*i, self.config.batch_size(i+1))
             sess.run(train_step, feed_dict=feed)
 
             # Test trained model
@@ -91,8 +124,4 @@ class Coref_clustter:
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--data_dir', type=str, default='./MNIST_data',
-                        help='Directory for storing data')
-    FLAGS = parser.parse_args()
-    tf.app.run()
+    Coref_clustter().main()
